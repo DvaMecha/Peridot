425630955bf5d8962b8c3bd28f5a372028b047c8
rolland_chen@126.com <rolland_chen@126.com>
2017-03-02 14:24:03 +0800

    new
    new

---
+++ PeridotJS.js
@@ -0,0 +1,39 @@
+/**
+ * @author Rolland Chen
+ */
+(function() {
+  var peridot = {
+    version: "0.0.1"
+  };
+
+  /**
+   * return a~b之间的随机数 f为true返回整数
+   * @param int a
+   * @param int b
+   * @param boolean f
+   */
+  peridot.random = function(a, b, f) {
+    var min = arguments[0] ? arguments[0] : 0;
+    var max = arguments[1] ? arguments[1] : 100;
+    var flag = arguments[2] ? arguments[2] : true;
+    var r = ~~((Math.random() * (b - a + 1)) + a);
+    if (f) {
+      r = Math.round(r);
+    }
+    return r;
+  };
+  /**
+   * 洗牌，随机排列数组a中的元素
+   * @param array a
+   */
+  peridot.shuffle = function(a) {
+    var m = a.length,
+      t, i;
+    while (m) {
+      i = Math.random() * m-- | 0;
+      t = a[m], a[m] = a[i], a[i] = t;
+    }
+    return array;
+  };
+  this.peridot = peridot;
+})();
